7758521327164a77a3999633aa0fdee66dcc7cad
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-08 18:15:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,14 @@ class thisbot(dogehouse.DogeClient):
       await self.create_room("DogeChatBot Testing Room")
     @dogehouse.event
     async def on_error(self,error):
-      await self.send("Error")
+      await self.send("Hey, that command doesn't exists, try to find the command on "+os.getenv("prefix")+"help")
     @dogehouse.command
     async def test0(self,ctx):
       await self.send("I'm @"+self.user.username)
+    @dogehouse.command
+    async def help(self,ctx):
+      await self.send(f"""DogeChatBot Command list
+{os.getenv("prefix")}help | Watch the command list
+{os.getenv("prefix")}test0 | Test0""")
 
 thisbot(os.getenv("token"), os.getenv("refresh_token"), prefix=os.getenv("prefix")).run()
